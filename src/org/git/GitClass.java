6c3cc5a0efa85172157c255bb05cccbec5cac2c2
ramyaganesan465 <ramyaganesan465@gmail.com>
2024-04-20 12:58:33 +0530

    success message

---
+++ src/org/git/GitClass.java
@@ -0,0 +1,44 @@
+package org.git;
+
+
+
+public class GitClass {
+	private void empid() {
+		 System.out.println("Employee id is D0123");
+	 }
+	 private void empname() {
+		 System.out.println("Employee name is Boobal");
+		 }
+	 private void empdob() {
+		 System.out.println("Employee dob is oct17/1990");
+	     }
+	 private void empphone() {
+		 System.out.println("Employee name is Boobalan");
+	 }
+	 private void empEmail() {
+		 System.out.println("Employee emai is Boobalanvv@gmail.com");
+	 }
+	 private void empaddress() {
+		 System.out.println("Employee address is salem");
+	 }
+	  
+	 public static void main (String[] args) {
+		 GitClass bv=new GitClass();
+		 bv.empid();
+		 bv.empname();
+		 bv.empdob();
+		 bv.empphone();
+		 bv.empEmail();
+		 bv.empaddress();
+	    
+	}
+		 
+				 
+	 
+	  
+	 
+	 
+
+
+
+}
